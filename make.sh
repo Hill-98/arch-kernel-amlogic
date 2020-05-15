d33b9740ac38f35c7772771eff7a7c5c3358e1e3
Zhong Lufan <lufanzhong@gmail.com>
2020-05-15 12:06:51 +0800

    Update GitHub action

--- make.sh
+++ make.sh
@@ -2,12 +2,17 @@
 pacman -Syu --noconfirm
 pacman -S --noconfirm --needed base-devel aarch64-linux-gnu-gcc
 
-cd "$(dirname $0)/linux-amlogic"
-
 export ARCH=arm64
 export CROSS_COMPILE=aarch64-linux-gnu-
 
-useradd cl-build
-echo "cl-build ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+echo "nobody ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
-sudo -u cl-build makepkg --syncdeps --noconfirm --config ../makepkg.conf
+repodir=$(dirname "$0")
+
+cp -r "$repodir/linux-amlogic" "/github/workflow/linux-amlogic"
+
+chown nobody:root /github/workflow/linux-amlogic
+
+cd /github/workflow/linux-amlogic || exit 1
+
+sudo -u nobody makepkg --syncdeps --noconfirm --config "$repodir/makepkg.conf"
