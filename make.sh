38b5bae347fbf817652d39b6dd1b3ad9fa8b94e6
Zhong Lufan <lufanzhong@gmail.com>
2020-05-15 12:44:10 +0800

    Update

--- make.sh
+++ make.sh
@@ -2,9 +2,6 @@
 pacman -Syu --noconfirm
 pacman -S --noconfirm --needed base-devel aarch64-linux-gnu-gcc
 
-export ARCH=arm64
-export CROSS_COMPILE=aarch64-linux-gnu-
-
 echo "nobody ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
 repodir=$(dirname "$0")
@@ -15,4 +12,4 @@ chown nobody:root /github/workflow/linux-amlogic
 
 cd /github/workflow/linux-amlogic || exit 1
 
-sudo -u nobody makepkg --syncdeps --noconfirm --config "$repodir/makepkg.conf"
+sudo -u nobody /usr/bin/env ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- makepkg --syncdeps --noconfirm --config "$repodir/makepkg.conf"
