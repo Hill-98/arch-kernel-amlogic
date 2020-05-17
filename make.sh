766c5b975568791e8cc9c67db68c20d919c03b4c
Zhong Lufan <lufanzhong@gmail.com>
2020-05-18 01:39:45 +0800

    Update linux-amlogic to 5.6.13

--- make.sh
+++ make.sh
@@ -2,14 +2,13 @@
 pacman -Syu --noconfirm
 pacman -S --noconfirm --needed base-devel aarch64-linux-gnu-gcc
 
-echo "nobody ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-
 builddir="$RUNNER_TEMP/linux-amlogic"
 cp -r "$GITHUB_WORKSPACE/linux-amlogic" "$builddir"
 chown nobody:root "$builddir"
 cd "$builddir" || exit 1
 
+echo "nobody ALL=(ALL) NOPASSWD: ALL" | tee -a /etc/sudoers
+
 sudo -u nobody /usr/bin/env ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- makepkg --syncdeps --noconfirm --config "$GITHUB_WORKSPACE/makepkg.conf"
 
-mkdir -p "$GITHUB_WORKSPACE/dist"
-cp ./*.pkg.tar.zst "$GITHUB_WORKSPACE/dist"
+cp ./*.pkg.tar.zst "$RUNNER_TEMP"
