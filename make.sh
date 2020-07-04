d92f4e14844f9e52626ccfcb4390f010e7039471
Zhong Lufan <lufanzhong@gmail.com>
2020-07-05 00:58:01 +0800

    Update linux-amlogic to 5.7.2-2

--- make.sh
+++ make.sh
@@ -7,9 +7,11 @@ cp -r "$GITHUB_WORKSPACE/linux-amlogic" "$builddir"
 chown nobody:root "$builddir"
 cd "$builddir" || exit 1
 
-echo "nobody ALL=(ALL) NOPASSWD: ALL" | tee -a /etc/sudoers
+echo "nobody ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
-sudo -u nobody /usr/bin/env ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- makepkg --syncdeps --noconfirm --config "$GITHUB_WORKSPACE/makepkg.conf"
+sudo -u nobody env \
+    ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- KBUILD_BUILD_HOST=github KBUILD_BUILD_USER=$GITHUB_REPOSITORY_OWNER \
+    makepkg --syncdeps --noconfirm --config "$GITHUB_WORKSPACE/makepkg.conf" || exit 1
 
 mkdir "$RUNNER_TEMP/pkg"
 mv ./*.pkg.tar.zst "$RUNNER_TEMP/pkg"
